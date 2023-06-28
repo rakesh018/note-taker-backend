2206f00de87425d56cc09357357e842873bf4e70
rakesh018 <rakhibodapatla@gmail.com>
2023-06-28 18:38:03 +0530

    added health check for server deployment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = require("./configs/db");
 app.use("/auth", authRoutes);
 app.use("/notes",notesRoutes);
 app.get("/health",(req,res)=>{
-  return res.send(200).send("Server is healthy.");
+  return res.status(200).send("Server is healthy.");
   //this is to check health of server
 })
 //connecting database
